d405607a4725b4af69c7800c624897a20fc5c22d
Aster <galaster@foxmail.com>
2016-03-05 12:08:35 +0800

    🌿 `token_print` function

---
+++ src/utils.rs
@@ -0,0 +1,26 @@
+use crate::{NotedownParser, NotedownRule};
+use colored::*;
+use pest::Parser;
+pub fn token_print(s: &str) {
+    let pairs = NotedownParser::parse(NotedownRule::program, s).unwrap_or_else(|e| panic!("{}", e));
+    // Because ident_list is silent, the iterator will contain idents
+    for pair in pairs {
+        // A pair is a combination of the rule which matched and a span of input
+        let rule = pair.as_rule();
+
+        println!("{} {:?}", "Rule:".green(), pair.as_rule());
+        println!("{} {:?}", "Span:".green(), pair.as_span());
+
+        if rule != NotedownRule::NEWLINE {
+            println!("{} {}", "Text:".green(), pair.as_str());
+        } else {
+            println!();
+        }
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            println!("    {} {:?}", "Inner:".cyan(), inner_pair.as_rule());
+            println!("           {:?}", inner_pair.as_span());
+        }
+    }
+}
