d861d4fb7feeefeb5364f9cf0c6a07b569e8f74a
Aster <galaster@foxmail.com>
2016-08-22 04:33:03 +0800

    🌿 Rethink the design of lists

--- projects/notedown-ast/src/nodes/list/mod.rs
+++ projects/notedown-ast/src/nodes/list/mod.rs
@@ -9,24 +9,96 @@ pub enum ListView {
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
-pub struct QuoteList {
-    style: String,
-    body: ASTNodes,
+pub enum ListPrefixSymbol {
+    /// Varies according to global settings
+    Default = 0,
+    /// ```note
+    /// 1.
+    /// 2.
+    /// ```
+    ArabicNumerals,
+    /// ```note
+    /// I.
+    /// II.
+    /// ```
+    RomanNumerals,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct ListItem {
+    prefix: ListPrefixSymbol,
+    rest: ASTNodes,
+}
+
+/// ## Quote List
+/// ```note
+/// > part1
+/// > part2
+///   part2
+/// > part3
+///
+/// > part4
+/// ```
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct QuoteList {
+    style: String,
+    children: Vec<Literal<ListItem>>,
+}
+
+/// ## Orderless List
+/// ```note
+/// 1.1. part1
+/// 1.2. part2
+///      part2
+/// 1.3. part3
+///
+/// 1.4. part4
+/// ```
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub struct OrderedList {
-    head: usize,
+    prefix: ListPrefixSymbol,
+    head: Vec<usize>,
     body: ASTNodes,
 }
 
+/// ## Orderless List
+/// ```note
+/// - part1
+/// - part2
+///   part2
+/// - part3
+///
+/// - part4
+/// ```
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub struct OrderlessList {
     body: ASTNodes,
 }
 
+/// ## Details List
+/// ```note
+/// >- summary part
+/// >- sum
+/// >  part2
+/// >  part3
+/// ```
+///
+/// ```html
+/// <details open>
+/// <summary>Want to ruin the surprise?</summary>
+/// <br>
+/// Well, you asked for it!
+/// </details>
+/// ```
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub struct DetailsList {
+    is_open: bool,
     summary: ASTNodes,
     body: ASTNodes,
+}
+
+impl Default for ListPrefixSymbol {
+    fn default() -> Self {
+        Self::Default
+    }
 }
