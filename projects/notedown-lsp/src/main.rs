b5e0173f8da1be04c2ab129fc352a7e8f3f79dab
Aster <galaster@foxmail.com>
2016-08-23 13:19:30 +0800

    🔮 List all completion kinds

--- projects/notedown-lsp/src/main.rs
+++ projects/notedown-lsp/src/main.rs
@@ -1,14 +1,44 @@
+use crate::completion::{complete_commands, list_completion_kinds};
 use tower_lsp::{jsonrpc::Result, lsp_types::*, Client, LanguageServer, LspService, Server};
+
+mod completion;
 
 #[derive(Debug)]
 struct Backend {
     client: Client,
 }
 
+fn c2() -> CompletionItem {
+    CompletionItem {
+        label: "<comment>".to_string(),
+        kind: Some(CompletionItemKind::Class),
+        detail: Some("Some comment text will not appear in the rendering result".to_string()),
+        documentation: Some(Documentation::MarkupContent(MarkupContent {
+            kind: MarkupKind::Markdown,
+            value: "`\\comment: something will not shown`
+                    `\\comment[some tips not shown]`\
+                    "
+                .to_string(),
+        })),
+        deprecated: None,
+        preselect: None,
+        sort_text: None,
+        filter_text: None,
+        insert_text: Some("<comment>$0<comment/>".to_string()),
+        insert_text_format: Some(InsertTextFormat::Snippet),
+        text_edit: None,
+        // text_edit: Some(CompletionTextEdit::Edit(TextEdit { range, new_text: "\\comment".to_string() })),
+        additional_text_edits: None,
+        command: None,
+        data: None,
+        tags: None,
+    }
+}
+
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        let server_info = ServerInfo { name: "ND LSP".to_string(), version: Some("V1".to_string()) };
+        let server_info = ServerInfo { name: "Notedown LSP".to_string(), version: Some("V1".to_string()) };
         let completion_trigger = vec![".", "\\", "[", "<"];
         let completion = CompletionOptions {
             resolve_provider: Some(false),
@@ -55,62 +85,30 @@ impl LanguageServer for Backend {
     }
 
     async fn completion(&self, cp: CompletionParams) -> Result<Option<CompletionResponse>> {
-        let _ = cp;
-        let end = cp.text_document_position.position;
-        let start = Position::new(end.line, end.character - 1);
-        let range = Range::new(start, end);
+        // self.client.log_message(MessageType::Info, format!("{:#?}", cp)).await;
+        let mut items = vec![];
 
+        match cp.context {
+            None => (),
+            Some(s) => match s.trigger_kind {
+                CompletionTriggerKind::Invoked => (),
+                CompletionTriggerKind::TriggerCharacter => match s.trigger_character.unwrap().as_str() {
+                    "\\" => items = complete_commands(),
+                    "<" => items = vec![c2()],
+                    "." => items = list_completion_kinds(),
+                    _ => (),
+                },
+                CompletionTriggerKind::TriggerForIncompleteCompletions => (),
+            },
+        };
         // const lineTextBefore = document.lineAt(position.line).text.substring(0, position.character);
         // const lineTextAfter = document.lineAt(position.line).text.substring(position.character);
-
-        let items = vec![
-            CompletionItem {
-                label: "\\comment".to_string(),
-                kind: Some(CompletionItemKind::Function),
-                detail: Some("Some comment text will not appear in the rendering result".to_string()),
-                documentation: Some(Documentation::MarkupContent(MarkupContent {
-                    kind: MarkupKind::Markdown,
-                    value: "`\\comment: something will not shown`
-                    `\\comment[some tips not shown]`\
-                    ".to_string(),
-                })),
-                deprecated: None,
-                preselect: None,
-                sort_text: None,
-                filter_text: None,
-                insert_text: None,
-                insert_text_format: None,
-                text_edit: Some(CompletionTextEdit::Edit(TextEdit { range, new_text: "\\comment".to_string() })),
-                additional_text_edits: None,
-                command: None,
-                data: None,
-                tags: None,
-            },
-            CompletionItem {
-                label: "<comment><comment/>".to_string(),
-                kind: Some(CompletionItemKind::Function),
-                detail: Some("Some comment text will not appear in the rendering result".to_string()),
-                documentation: Some(Documentation::MarkupContent(MarkupContent {
-                    kind: MarkupKind::Markdown,
-                    value: "`\\comment: something will not shown`
-                    `\\comment[some tips not shown]`\
-                    ".to_string(),
-                })),
-                deprecated: None,
-                preselect: None,
-                sort_text: None,
-                filter_text: None,
-                insert_text: None,
-                insert_text_format: None,
-                text_edit: Some(CompletionTextEdit::Edit(TextEdit { range, new_text: "\\comment".to_string() })),
-                additional_text_edits: None,
-                command: None,
-                data: None,
-                tags: None,
-            },
-        ];
         Ok(Some(CompletionResponse::Array(items)))
     }
+
+    // async fn completion_resolve(&self, params: CompletionItem) -> Result<CompletionItem> {
+    //    Ok(params)
+    //}
 
     async fn hover(&self, _: HoverParams) -> Result<Option<Hover>> {
         Ok(Some(Hover { contents: HoverContents::Scalar(MarkedString::String("You're hovering!".to_string())), range: None }))
