610c39522a452a6139755e8abd5e51a9c515dd5c
Aster <galaster@foxmail.com>
2016-09-08 07:33:51 +0800

    🎀 Support external document editing

--- projects/notedown-lsp/src/completion/self_close.rs
+++ projects/notedown-lsp/src/completion/self_close.rs
@@ -4,14 +4,14 @@ pub fn build_self_close(cmd: &str, short: &str, long: &str) -> CompletionItem {
     let doc = MarkupContent { kind: MarkupKind::Markdown, value: String::from(long) };
     CompletionItem {
         label: format!("<{}>", cmd),
-        kind: Some(CompletionItemKind::Function),
+        kind: Some(CompletionItemKind::Class),
         detail: Some(String::from(short)),
         documentation: Some(Documentation::MarkupContent(doc)),
         deprecated: None,
         preselect: None,
         sort_text: None,
         filter_text: None,
-        insert_text: Some(format!("<{cmd}$0/>", cmd = cmd)),
+        insert_text: Some(format!("<{cmd}$0/>$1", cmd = cmd)),
         insert_text_format: Some(InsertTextFormat::Snippet),
         text_edit: None,
         // text_edit: Some(CompletionTextEdit::Edit(TextEdit { range, new_text: "\\comment".to_string() })),
