de9883758242a2605afbda691a4e0a62aed20ccf
Aster <galaster@foxmail.com>
2016-09-09 07:47:54 +0800

    🌿 Capitalize the first letter

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,3 +7,13 @@ pub use html_escape::*;
 pub use table_align::*;
 pub use text_align::*;
 pub use text_escape::*;
+
+
+pub fn capitalize_first_letter(text: impl AsRef<str>) -> String {
+    let text = text.as_ref();
+    let mut c = text.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
