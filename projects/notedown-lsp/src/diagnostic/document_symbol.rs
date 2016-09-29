5dd2ef49b438eb3e361e4d85d7ab73f10238aaeb
Aster <galaster@foxmail.com>
2016-09-29 11:29:19 +0800

    🔧 Fix position of toc jump

--- projects/notedown-lsp/src/diagnostic/document_symbol.rs
+++ projects/notedown-lsp/src/diagnostic/document_symbol.rs
@@ -1,20 +1,13 @@
-use tower_lsp::lsp_types::{DocumentSymbolParams, DocumentSymbol, SymbolKind, DocumentSymbolResponse};
+use tower_lsp::lsp_types::{DocumentSymbolParams, DocumentSymbolResponse};
 use crate::io::read_url;
 use notedown_parser::parse;
 use crate::diagnostic::ToToc;
 
 #[allow(deprecated)]
 pub fn document_symbol_provider(args: DocumentSymbolParams) -> Option<DocumentSymbolResponse> {
-    let toc = parse(&read_url(&args.text_document.uri)).to_toc();
-
-    let tree: Vec<DocumentSymbol> = vec![DocumentSymbol {
-        name: String::from("TOC"),
-        detail: Some(String::from("Table of Contents")),
-        kind: SymbolKind::Package,
-        deprecated: None,
-        range: Default::default(),
-        selection_range: Default::default(),
-        children: Some(vec![toc]),
-    }];
-    Some(DocumentSymbolResponse::Nested(tree))
+    let nested = match parse(&read_url(&args.text_document.uri)).to_toc().children {
+        Some(v) => v,
+        None => vec![]
+    };
+    Some(DocumentSymbolResponse::Nested(nested))
 }
