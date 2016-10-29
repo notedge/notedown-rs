08d417eb3ae96ae926c818d097df28b622913430
Aster <galaster@foxmail.com>
2016-10-30 05:16:31 +0800

    🔧 Fix clipboard not Send

--- projects/notedown-lsp/src/commands/mod.rs
+++ projects/notedown-lsp/src/commands/mod.rs
@@ -1,57 +1,58 @@
-use clipboard::ClipboardProvider;
-use clipboard::ClipboardContext;
-use tower_lsp::lsp_types::*;
+use cli_clipboard::{ClipboardContext, ClipboardProvider};
 use serde_json::Value;
-use std::collections::HashSet;
-use tower_lsp::Client;
-
+use std::{collections::HashSet, lazy::SyncLazy};
+use tower_lsp::{lsp_types::*, Client};
 
 pub fn server_commands() -> ExecuteCommandOptions {
-    let commands = get_all().iter().map(|s|s.to_string()).collect();
-    ExecuteCommandOptions {
-        commands,
-        work_done_progress_options: Default::default(),
-    }
+    let commands = SERVER_COMMANDS.iter().map(|s| s.to_string()).collect();
+    ExecuteCommandOptions { commands, work_done_progress_options: Default::default() }
 }
 
-fn get_all() -> HashSet<&'static str> {
+static SERVER_COMMANDS: SyncLazy<HashSet<&'static str>> = SyncLazy::new(|| {
     let mut s = HashSet::new();
     s.insert("vscode-notedown.injectPaste");
     s.insert("vscode-notedown.rawPaste");
     s.insert("vscode-notedown.image.save2local");
     return s;
-}
+});
 
-
-
-pub fn command_provider(p: ExecuteCommandParams,c: &Client) -> Option<Value> {
-    let _ = p;
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-    if let Ok(s) = ctx.get_contents() {
-        c.show_message(MessageType::Info, s);
-        // return Some(Value::String(s))
-    }
-/*
-    c.apply_edit();
-
-
-    WorkspaceEdit {
-        changes: None,
-        document_changes: Some(DocumentChanges::Edits(vec![]) )
+pub async fn command_provider(p: ExecuteCommandParams, c: &Client) -> Option<Value> {
+    c.show_message(MessageType::Log, format!("{:#?}", p.command)).await;
+    let mut ctx: ClipboardContext = match ClipboardProvider::new() {
+        Ok(o) => o,
+        Err(e) => {
+            c.show_message(MessageType::Error, e).await;
+            return None;
+        }
     };
 
-    TextDocumentEdit {
-        text_document: VersionedTextDocumentIdentifier { uri: (), version: None },
-        edits: vec![
-
-        ]
+    let s = match ctx.get_contents() {
+        Ok(o) => o,
+        Err(e) => {
+            c.show_message(MessageType::Error, e).await;
+            return None;
+        }
     };
-    TextEdit{
-        range: Default::default(),
-        new_text: "".to_string()
-    };
-*/
+    c.show_message(MessageType::Info, s).await;
+    return None;
 
-
-    return None
+    // c.apply_edit();
+    //
+    //
+    // WorkspaceEdit {
+    // changes: None,
+    // document_changes: Some(DocumentChanges::Edits(vec![]) )
+    // };
+    //
+    // TextDocumentEdit {
+    // text_document: VersionedTextDocumentIdentifier { uri: (), version: None },
+    // edits: vec![
+    //
+    // ]
+    // };
+    // TextEdit{
+    // range: Default::default(),
+    // new_text: "".to_string()
+    // };
+    //
 }
--- projects/notedown-lsp/src/hint/document_symbol.rs
+++ projects/notedown-lsp/src/hint/document_symbol.rs
@@ -1,17 +1,13 @@
 use crate::{diagnostic::ToToc, io::read_url};
-use notedown_parser::{parse, ParserConfig, AST, Error};
+use notedown_parser::ParserConfig;
 use tower_lsp::lsp_types::{DocumentSymbolParams, DocumentSymbolResponse};
 
 #[allow(deprecated)]
 pub fn document_symbol_provider(args: DocumentSymbolParams) -> Option<DocumentSymbolResponse> {
-
-
     let cfg = ParserConfig::default();
     let ast = match cfg.parse(&read_url(&args.text_document.uri)) {
-        Ok(o) => {o},
-        Err(_) => {
-            return None
-        }
+        Ok(o) => o,
+        Err(_) => return None,
     };
 
     let nested = match ast.to_toc().children {
